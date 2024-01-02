ef9ac4713fdcb38e49160447783e1a5fabcaa223
Shoaib <shoaibs411@gmail.com>
2024-01-03 01:06:34 +0530

    Provisioning VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source  =    "https://github.com/Shoaibs411/tf-module-vpc.git"
+    source  =    "git::https://github.com/Shoaibs411/tf-module-vpc.git"
 
     VPC_CIDR = var.VPC_CIDR
 }
