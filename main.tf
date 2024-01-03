f442beca32c5aaa035bdba080479fe3e51933535
Shoaib <shoaibs411@gmail.com>
2024-01-04 01:52:53 +0530

    Updated terrafile

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 module "vpc"{
-  source              = "git::https://github.com/Shoaibs411/tf-module-vpc.git"
+  # source              = "git::https://github.com/Shoaibs411/tf-module-vpc.git"
+  
+  source              = "./vendor/modules/vpc"
+
   ENV                 = var.ENV
   VPC_CIDR            = var.VPC_CIDR
 }
