982596bcf0010286d99a875604b93b3ed78beff6
Shoaib <shoaibs411@gmail.com>
2024-01-04 01:57:36 +0530

    Provisioning VPC - Updated Terrafile command in Jenkins FIle

--- main.tf
+++ main.tf
@@ -5,4 +5,6 @@ module "vpc"{
 
   ENV                 = var.ENV
   VPC_CIDR            = var.VPC_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
