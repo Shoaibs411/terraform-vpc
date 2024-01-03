bcceb5b804c066c814eef169d15e67ebe913bf4a
Shoaib <shoaibs411@gmail.com>
2024-01-04 01:09:44 +0530

    Provisioning VPC - updated Terrafile in env-dev & env-prod

--- main.tf
+++ main.tf
@@ -2,6 +2,4 @@ module "vpc"{
   source              = "git::https://github.com/Shoaibs411/tf-module-vpc.git"
   ENV                 = var.ENV
   VPC_CIDR            = var.VPC_CIDR
-  # PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  # PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
